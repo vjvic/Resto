172f143f265a7dec2ee9b25631d2d066d3d694bb
vjvic <victorsulit0@gmail.com>
2021-10-12 10:13:57 +0800

    fix error

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,35 @@
 import Hero from "components/Hero/Hero";
 import InfoSection from "components/InfoSection/InfoSection";
-import { infoData } from "components/InfoSection/InfoObj";
+/* import { infoData } from "components/InfoSection/InfoObj"; */
 import BookCTASection from "components/BookCTASection/BookCTASection";
 import Popular from "components/Popular/Popular";
+
+const infoData = [
+  {
+    id: "info1",
+    title: "We Make Sure Your Food Arrive On Time",
+    description:
+      "Agile drink the Kool-aid, for nail jelly to the hothouse wall, for deploy, and nail it down when does this sunset? . They have downloaded gmail and seems to be working for now future-proof pass the mayo, appeal to the client, sue the vice president , touch base. Looks great, can we try it a different way what the put a record on and see who dances bleeding edge.",
+    imgStart: true,
+    img: "/info-image/info1.jpg",
+  },
+  {
+    id: "info2",
+    title: "High Quality Food Material",
+    description:
+      "Agile drink the Kool-aid, for nail jelly to the hothouse wall, for deploy, and nail it down when does this sunset? . They have downloaded gmail and seems to be working for now future-proof pass the mayo, appeal to the client, sue the vice president , touch base. Looks great, can we try it a different way what the put a record on and see who dances bleeding edge.",
+    imgStart: false,
+    img: "/info-image/info2.jpg",
+  },
+  {
+    id: "info3",
+    title: "Various Menus You Can Pick",
+    description:
+      "Agile drink the Kool-aid, for nail jelly to the hothouse wall, for deploy, and nail it down when does this sunset? . They have downloaded gmail and seems to be working for now future-proof pass the mayo, appeal to the client, sue the vice president , touch base. Looks great, can we try it a different way what the put a record on and see who dances bleeding edge.",
+    imgStart: true,
+    img: "/info-image/info3.jpg",
+  },
+];
 
 export default function Home() {
   return (
