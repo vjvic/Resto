eb23cdc3043482715dfeeabbf8f996d70f01a79d
vjvic <victorsulit0@gmail.com>
2021-10-12 09:44:14 +0800

    Refactor some codes

--- pages/book.js
+++ pages/book.js
@@ -1,9 +1,62 @@
 import { useState } from "react";
 import { Paragraph } from "styles/Section";
-import { BookContainer, BookImg, BookTitle, InputWrapper } from "./Book.styles";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { Button } from "styles/Button";
+import styled from "styled-components";
+
+const BookContainer = styled.section`
+  min-height: 100vh;
+  padding: 40px 0;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 4rem;
+
+  ${Button} {
+    margin-top: 1.5rem;
+    width: 100%;
+  }
+
+  @media ${({ theme }) => theme.breakpoints.md} {
+    display: block;
+  }
+`;
+
+const BookImg = styled.div`
+  img {
+    margin-left: auto;
+  }
+
+  @media ${({ theme }) => theme.breakpoints.md} {
+    display: none;
+  }
+`;
+
+const BookTitle = styled.h1`
+  padding: 1rem 0;
+`;
+
+const InputWrapper = styled.div`
+  margin: 1rem 0;
+
+  input {
+    width: 100%;
+    display: block;
+    border: none;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.5);
+    background: none;
+
+    &:focus {
+      outline: none;
+      border-bottom: 1px solid ${({ theme }) => theme.color.primary};
+    }
+  }
+
+  label {
+    color: ${({ theme }) => theme.text.secondary};
+    opacity: 0.8;
+  }
+`;
 
 const Book = () => {
   const [startDate, setStartDate] = useState(new Date());
